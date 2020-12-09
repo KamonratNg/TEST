1e246a8526a148928f3e2c4d41648629407d31e1
KamonratNg <kamonrat.nga@gmail.com>
2020-12-10 01:45:45 +0700

    update range

--- guitest.py
+++ guitest.py
@@ -33,7 +33,24 @@ canvas1.create_window(200, 200, window=entry2)
 def bmical ():
     answerbmi = float(entry2.get()) / float(entry1.get())**2
     answer.config(text="Result is %d" % answerbmi)
-    return
+
+    if (answerbmi < 10 or answerbmi > 50):
+        label4.config(text = "OMG You are not a HUMAN", bg='red')
+
+    elif (answerbmi < 18.5):
+        label4.config(text = "Underweight", bg='orage')
+        
+    elif (answerbmi >= 18.5 and answerbmi <= 24.9):
+        label4.config(text = "Normal weight", bg='green')
+
+    elif (answerbmi >= 25 and answerbmi <= 29.9):
+        label4.config(text = "Overweight", bg='pink')
+
+    else:
+        label4.config(text = "Obesity", bg='purple')
+
+
+    
 
 
     
@@ -42,6 +59,9 @@ canvas1.create_window(200, 300, window=button1)
 
 answer = tk.Label(root, text = '')
 canvas1.create_window(200, 350, window=answer)
+
+label4 = tk.Label(root, text = '')
+canvas1.create_window(200, 380, window=label4)
 
 
 root.mainloop()
